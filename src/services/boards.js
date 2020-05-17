3a48c3c6f61891b41060d0ea33e56e4d33c3e472
Jesús Martínez Saugar <jesus.martinez.saugar@gmail.com>
2020-05-17 15:55:44 +0200

    feat: setup basic board users connection handling

--- src/services/boards.js
+++ src/services/boards.js
@@ -1,25 +1,104 @@
 import io from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
 import { boardsMessages } from '../constants';
 
-let server;
+let server; // Socketio server instance
+const boards = {}; // Object that contains data for boards (including users)
+const socket2board = {}; // Required to keep link between socket.id and board
 
 /**
- * Handler for client join request.
+ * Create a new user and add it to a board.
+ * If the board does not exist, it is created.
+ *
+ * @param {String} boardId Id of the board.
+ * @param {String} socketid Id of the socket for the user.
+ * @param {String} boardName Name of the board (in case of board creation).
+ * @param {String} userName Name of the new user.
+ */
+const addUserToBoard = (boardId, socketId, boardName, userName) => {
+  const newUser = {
+    id   : uuidv4(),
+    name : userName,
+    socketId,
+  };
+
+  if (!boards[boardId]) {
+    boards[boardId] = {
+      name         : boardName,
+      creationDate : new Date(),
+      users        : [newUser],
+    };
+  } else {
+    boards[boardId].users.push(newUser);
+  }
+
+  socket2board[socketId] = boardId;
+};
+
+/**
+ * Get all users on a given board.
+ *
+ * @param {String} boardId Id of the board.
+ *
+ * @return {Array<Object>} Array of users.
+ */
+const getBoardUsers = (boardId) => (
+  (boardId && boards[boardId]) ? boards[boardId].users : []
+);
+
+/**
+ * Handler for user join request.
  *
  * @param {*} data { boardId }
  */
-const onJoin = (client, { boardId }) => {
-  // @todo if no boardId, etc, answer client with rejection
-  client.join(boardId, () => {
-    console.log('!!!.joined', boardId);
+const onJoin = (socket, { boardId, userName, boardName }) => {
+  // @todo if no boardId, etc, answer user with rejection
+  socket.join(boardId, () => {
+    // @todo same with userName?
+    if (!boardId) {
+      return;
+    }
 
-    // Confirm the client he has joined the room
-    client.emit(boardsMessages.meJoined, { boardId }); // @todo differentiate this join ack from others
+    // Confirm the user he has joined the room
+    socket.emit(boardsMessages.meJoined, { boardId, users : getBoardUsers(boardId) });
 
-    // Tell all other room clients that a new client has connected to the room
-    client.to(boardId).emit(boardsMessages.otherJoined, { boardId });
+    // Add new user to board (also creating board if it does not exist)
+    addUserToBoard(boardId, socket.id, boardName, userName);
+
+    // Tell all other room users that a new user has connected to the room
+    socket.to(boardId).emit(boardsMessages.otherJoined, { boardId });
   });
+};
+
+/**
+ * Handle disconnection of an user.
+ *
+ * @param {String} socketId Id of the socket for the disconnected user.
+ */
+const onDisconnect = (socketId) => {
+  if (!socket2board[socketId]) {
+    return;
+  }
+
+  const boardId = socket2board[socketId];
+  let userId;
+
+  // Get user id and remove user from board users list
+  boards[boardId].users = boards[boardId].users.filter((user) => {
+    if (user.socketId === socketId) {
+      userId = user.id;
+      return false;
+    }
+
+    return true;
+  });
+
+  // Tell other users in the board that this user has left
+  server.to(boardId).emit(boardsMessages.otherLeft, { userId });
+
+  // Remove socket-board link
+  delete socket2board[socketId];
 };
 
 /**
@@ -28,10 +107,9 @@ const onJoin = (client, { boardId }) => {
 const init = () => {
   server = io({ serveClient : false }); // @todo origins
 
-  server.on('connection', (client) => {
-    console.log('!!!.connected');
-
-    client.on(boardsMessages.join, onJoin.bind(this, client));
+  server.on('connection', (socket) => {
+    socket.on(boardsMessages.join, onJoin.bind(this, socket));
+    socket.on('disconnect', onDisconnect.bind(this, socket.id));
   });
 
   server.listen(process.env.SOCKETIO_PORT);
